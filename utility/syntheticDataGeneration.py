420a1c32d54b0dd44e360d11191f780c56f11cc6
HaohanWang <haohanw@andrew.cmu.edu>
2016-09-22 08:51:42 -0400

    data generate

--- utility/syntheticDataGeneration.py
+++ utility/syntheticDataGeneration.py
@@ -17,9 +17,9 @@ def generateData(seed):
     sig = 1
 
     we = 0.01
-    wh = 5
-    # wg = [0.5, 0.25, 0.125, 0.0625]
-    wg = [1, 1, 1, 1]
+    wh = 1
+    wg = [0.5, 0.25, 0.125, 0.0625]
+    # wg = [1, 1, 1, 1]
 
     center = np.random.uniform(0, 1, [g,p])
     sample = n/g
