f3803fbf006ffa60e3fdd1fc0da7ae2fe41384cd
HaohanWang <haohanw@andrew.cmu.edu>
2016-09-30 13:44:51 -0400

    error test

--- scripts/syntheticRun.py
+++ scripts/syntheticRun.py
@@ -18,16 +18,30 @@ def syntheticRunning():
         print '======================'
         print 'seed:', i
         generateData(i)
-        for j in range(4):
-            run_synthetic(j)
-        rs = []
-        for j in range(4):
-            r = evaluateSynthetic(j, roc)
-            rs.extend(r)
-        rss.append(rs)
+        try:
+            for j in range(4):
+                run_synthetic(j)
+            rs = []
+            for j in range(4):
+                r = evaluateSynthetic(j, roc)
+                rs.extend(r)
+            rss.append(rs)
+        except:
+            print 'Error'
         print '======================'
     print rss
     np.savetxt('Syn_meta.csv', np.array(rss), delimiter=',')
+
+def syntheticSingleRun(seed):
+    from utility.syntheticDataGeneration import generateData
+    from sLMMn.sLMMn import run_synthetic
+    roc = True
+    generateData(seed)
+    for j in range(4):
+        run_synthetic(j)
+    for j in range(4):
+        r = evaluateSynthetic(j, roc)
+        print r
 
 
 if __name__ == '__main__':
