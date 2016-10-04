6f2e31eb80bef0c71138a5e1d1bb2fe03f62affc
HaohanWang <haohanw@andrew.cmu.edu>
2016-10-04 10:32:46 -0400

    toy data run (this should work)

--- utility/toyDataGeneration.py
+++ utility/toyDataGeneration.py
@@ -17,6 +17,81 @@ def normalize(x):
     s = np.std(x)
     return (x - m) / s
 
+def generateDataTest(seed):
+    from matplotlib import pyplot as plt
+    np.random.seed(seed)
+    dense = 0.05
+    n = 100
+    p = 1000
+    zp = 5
+    g = 5
+    sig = 1
+    sigC = 100
+    we = 0.01
+    center = np.random.uniform(0, 1, [g, p])
+    center2 = np.random.uniform(0, 1, [g, zp])
+    sample = n / g
+    X = []
+    Z = []
+    for i in range(g):
+        x = np.random.multivariate_normal(center[i, :], sig * np.diag(np.ones([p, ])), size=sample)
+        X.extend(x)
+        for j in range(sample):
+            Z.append(center2[i])
+    X = np.array(X)
+    Z = np.array(Z)
+    print X.shape
+    X[X > -1] = 1
+    X[X <= -1] = 0
+    featureNum = int(p * dense)
+    idx = scipy.random.randint(0, p, featureNum).astype(int)
+    idx = sorted(idx)
+    w = 1 * np.random.normal(0, 1, size=featureNum)
+    ypheno = scipy.dot(X[:, idx], w)
+    ypheno = (ypheno - ypheno.mean()) / ypheno.std()
+    ypheno = ypheno.reshape(ypheno.shape[0])
+    error = np.random.normal(0, 1, n)
+
+    C = np.dot(Z, Z.T)
+    # C = centralize(C)
+    # plt.imshow(C)
+    # plt.show()
+    C = normalize(C)
+    causal = np.array(zip(idx, w))
+    np.savetxt('../toyData/causal.csv', causal, '%5.2f', delimiter=',')
+
+    y = we * error + normalize(ypheno)
+
+    yK1 = np.random.multivariate_normal(ypheno, sigC * C, size=1)
+    yK1 = yK1.reshape(yK1.shape[1])
+    yK1 = we * error + normalize(yK1)
+
+    C2 = np.dot(C, C)
+    # plt.imshow(C2)
+    # plt.show()
+    C2 = normalize(C2)
+    yK2 = np.random.multivariate_normal(ypheno, sigC * C2, size=1)
+    yK2 = yK2.reshape(yK2.shape[1])
+    yK2 = we * error + normalize(yK2)
+
+    n = np.random.randint(3, 4)
+    Ct = C
+    for i in range(n):
+        C = np.dot(Ct, C)
+    # plt.imshow(C)
+    # plt.show()
+    C = normalize(C)
+    yKn = np.random.multivariate_normal(ypheno, sigC * C, size=1)
+    yKn = yKn.reshape(yKn.shape[1])
+    yKn = we * error + normalize(yKn)
+
+    x = xrange(len(y))
+    plt.scatter(x, y, color='g')
+    plt.scatter(x, yK1, color='r')
+    plt.scatter(x, yK2, color='b')
+    plt.scatter(x, yKn, color='m')
+    plt.show()
+
 
 def generateData(seed):
     np.random.seed(seed)
@@ -28,9 +103,9 @@ def generateData(seed):
     zp = 5
     g = 5
     sig = 1
-    sigC = 1
+    sigC = 100
 
-    we = 1
+    we = 0.01
 
     center = np.random.uniform(0, 1, [g, p])
     center2 = np.random.uniform(0, 1, [g, zp])
@@ -47,8 +122,8 @@ def generateData(seed):
     Z = np.array(Z)
     print X.shape
 
-    # X[X > -1] = 1
-    # X[X <= -1] = 0
+    X[X > -1] = 1
+    X[X <= -1] = 0
 
     featureNum = int(p * dense)
     idx = scipy.random.randint(0, p, featureNum).astype(int)
@@ -61,6 +136,7 @@ def generateData(seed):
 
     C = np.dot(Z, Z.T)
     # C = centralize(C)
+    C = normalize(C)
 
     Kva, Kve = np.linalg.eigh(C)
     np.savetxt('../toyData/Kva.csv', Kva, delimiter=',')
@@ -78,14 +154,17 @@ def generateData(seed):
     np.savetxt('../toyData/K1/y.csv', yK1, '%5.2f', delimiter=',')
 
     C2 = np.dot(C, C)
+    C2 = normalize(C2)
     yK2 = np.random.multivariate_normal(ypheno, sigC * C2, size=1)
     yK2 = yK2.reshape(yK2.shape[1])
     yK2 = we * error + normalize(yK2)
     np.savetxt('../toyData/K2/y.csv', yK2, '%5.2f', delimiter=',')
 
-    n = np.random.randint(1, 4)
+    n = np.random.randint(3, 4)
+    Ct = C
     for i in range(n):
-        C = np.dot(C, C)
+        C = np.dot(Ct, C)
+    C = normalize(C)
     yKn = np.random.multivariate_normal(ypheno, sigC * C, size=1)
     yKn = yKn.reshape(yKn.shape[1])
     yKn = we * error + normalize(yKn)
@@ -122,4 +201,4 @@ def generateData(seed):
 
 
 if __name__ == '__main__':
-    generateData(1)
+    generateDataTest(0)
