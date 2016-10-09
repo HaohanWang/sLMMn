f3d2f3c4b9a3c9ae4a4a522b7314112c3572014d
HaohanWang <haohanw@andrew.cmu.edu>
2016-10-09 14:25:15 -0400

    covariance cal

--- realDataStudy/covarianceCalculation.py
+++ realDataStudy/covarianceCalculation.py
@@ -1,13 +1,15 @@
 __author__ = 'Haohan Wang'
 
 import sys
+
 sys.path.append('../')
 
 from utility.simpleFunctions import *
 
+
 def calculateCovariance():
     path = '/home/haohanw/FaSTLMM_K2_Sparsity/data/'
-    X = np.loadtxt(path + 'athaliana.snps.chrom1.csv', delimiter=',')
+    X = np.loadtxt(path + 'athaliana2.snps.csv', delimiter=',')
     K = np.dot(X, X.T)
     Kva, Kve = np.linalg.eigh(K)
     np.savetxt('../ATData/Kva.csv', Kva, delimiter=',')
@@ -15,10 +17,11 @@ def calculateCovariance():
     np.savetxt('../ATData/K.csv', K, delimiter=',')
     Ks = []
     Ks.append(K)
-    K2 = np.dot(K,K)
+    K2 = np.dot(K, K)
     Ks.append(K2)
     Ks.append(np.dot(K2, K))
     np.save('../ATData/Ks', Ks)
+
 
 def visualizeCovariance():
     from matplotlib import pyplot as plt
@@ -30,5 +33,15 @@ def visualizeCovariance():
         plt.show()
 
 
+def visualizeEigenValue():
+    from matplotlib import pyplot as plt
+    S = np.loadtxt('../ATData/Kva.csv', delimiter=',')
+    x = np.array(xrange(S.shape[0]))
+    plt.scatter(x[:-1], rescale(S[:-1]), color='y', marker='+')
+    plt.scatter(x[:-1], rescale(np.power(S[:-1], 2)), color='b', marker='+')
+    plt.scatter(x[:-1], rescale(np.power(S[:-1], 3)), color='m', marker='+')
+    plt.show()
+
+
 if __name__ == '__main__':
-    calculateCovariance()
+    visualizeCovariance()
